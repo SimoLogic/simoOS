481c33b71c36267dd6c661e3b3a98a27cd8a647d
itsupportsimologic <itsupportaccount@simologic.com>
2026-03-29 21:04:47 -0400

    Update layout.tsx
    auth commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-    title: "HOPSY — HOMESI Operating System",
+    title: "HOPSY - HOMESI Operating System",
     description: "Business Operating System for Mortgage Professionals",
 };
 
